8568f5925c4210d6e1ecc3e93d320382c52195be
wuchang <kkang0820qus@gmail.com>
2023-07-17 10:53:02 +0900

    boj17175, 5427, 5547 추가

--- src/Baekjoon/boj7662/Main.java
+++ src/Baekjoon/boj7662/Main.java
@@ -3,10 +3,7 @@ package Baekjoon.boj7662;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
     static int T, K;
